a6b410a91f66938ac75dc6f4504faafc78ebea93
Precious Akpan <precious.akpan2000@gmail.com>
2021-10-26 19:14:16 +0100

    adding database connection

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-objective-25844",
-    user: "Nene",
-    password: "serendipity",
-    database: "smart-brain",
+    host: "process.env.DATABASE_URL",
+    ssl: true,
   },
 });
 
