42c0e7eaf84f6e74d18551e9e84a178c58fa4249
Precious Akpan <precious.akpan2000@gmail.com>
2021-10-26 18:53:49 +0100

    updating database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-objective-25844",
     user: "Nene",
     password: "serendipity",
     database: "smart-brain",
